24de5caeb0557a0530fb2121bfbf88d2623892c7
Phil Housley <undeconstructed@gmail.com>
2020-03-13 23:43:10 +0000

    N

--- eo.search.min.f182324d01f470ce942456d3fa5288c1b94684963becc5142c8e317bd332615a.js
+++ eo.search.min.f182324d01f470ce942456d3fa5288c1b94684963becc5142c8e317bd332615a.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/vortejo/flexsearch.min.js');loadScript('/vortejo/eo.search-data.min.4f83a9e838218f1f12e3a38305e7d5b9749b9f9b7d514cd29a23994f8d808845.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/vortejo/flexsearch.min.js');loadScript('/vortejo/eo.search-data.min.4c73106a866a08a982ea11c96990c4ebd931c0c66adfb21faa5288fbf279579e.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
