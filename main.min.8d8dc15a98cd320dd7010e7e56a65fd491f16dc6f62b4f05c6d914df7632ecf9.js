9d5f66bf9490637c7c641d05045204ac097b7e80
Phil Housley <undeconstructed@gmail.com>
2020-03-15 23:57:22 +0000

    Regen

---
+++ main.min.8d8dc15a98cd320dd7010e7e56a65fd491f16dc6f62b4f05c6d914df7632ecf9.js
@@ -0,0 +1,7 @@
+(function(w){let eS=document.querySelector("#ŝirmilo")
+let eM=eS.querySelector(".montrilo")
+eS.addEventListener("click",()=>{eS.hidden=true})
+function montru(e){if(eM.firstChild){eM.removeChild(eM.firstChild)}
+eM.appendChild(e)
+eS.hidden=false}
+w.montru=montru})(window)
