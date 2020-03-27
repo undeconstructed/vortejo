897b60a1244269f66655be33cf5d4aa498dbd0b5
Phil Housley <undeconstructed@gmail.com>
2020-03-27 18:15:11 +0000

    Regen

---
+++ main.8118dd44192b835c7b008ed5abb256c83e1590b90645d213924489cdb4719c7e.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let eS = document.querySelector("#ŝirmilo")
+  let eM = eS.querySelector(".montrilo")
+
+  eS.addEventListener("click", (e) => {
+    if (e.target == eS) {
+      eS.hidden = true
+    }
+  })
+
+  function montru(e) {
+    malplenigu(eM)
+    eM.append(e)
+    eS.hidden = false
+  }
+
+  window.montru = montru
+})
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.addEventListener('selectionchange', () => {
+//     console.log(document.getSelection());
+//   })
+// })
+
+function malplenigu(e) {
+  while (e.firstChild) {
+    e.removeChild(e.firstChild)
+  }
+}
+
+function montruEraro(e, eraro) {
+  console.log(eraro)
+  montruTeksto(e, 'err: ' + eraro)
+}
+
+function montruTeksto(e, t) {
+  if (t === null || t === undefined) {
+    e.textContent = ''
+    e.hidden = true
+  } else {
+    e.textContent = t
+  }
+}
+
+let _eo = null;
+
+const eo = (function() {
+  let eo = null
+  let dulo = null
+
+  function komprenuVorton(teksto) {
+    if (!dulo) {
+      dulo = new Dividulo(eo.radikoj)
+    }
+    return dulo.komprenuVorton(teksto)
+  }
+
+  function kreuObj(radikoj, fakoj) {
+    return eo = {
+      radikoj: radikoj,
+      r2: null,
+      fakoj: fakoj,
+      komprenuVorton: komprenuVorton,
+      thing: function() {
+        console.log('thing')
+      }
+    }
+  }
+
+  function get(fonto) {
+    if (!eo) {
+      let fr = fetch(fonto + '/radikoj.json').then(rez => rez.json())
+      let ff = fetch(fonto + '/fakoj.json').then(rez => rez.json())
+      eo = Promise.all([fr, ff]).then(([r, f]) => kreuObj(r, f))
+    }
+    eo.then(e => { _eo = e; })
+    return eo
+  }
+
+  return get
+})();
--- main.min.72da15ee44001133496bbea8fd5426fe35a4ff083442926b89e5098288db4fe3.js
+++
@@ -1,7 +0,0 @@
-(function(w){let eS=document.querySelector("#ŝirmilo")
-let eM=eS.querySelector(".montrilo")
-eS.addEventListener("click",(e)=>{if(e.target==eS){eS.hidden=true}})
-function montru(e){if(eM.firstChild){eM.removeChild(eM.firstChild)}
-eM.appendChild(e)
-eS.hidden=false}
-w.montru=montru})(window)
